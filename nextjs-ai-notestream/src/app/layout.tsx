50559b968d55262e39155b2d9f10c51415dfd522
kaptainkev <42627348+kaptainkev@users.noreply.github.com>
2024-07-31 15:22:28 -0400

    Note Stream changes

--- nextjs-ai-notestream/src/app/layout.tsx
+++ nextjs-ai-notestream/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Inter as FontSans } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
